ee7f715a2c3b27b0151c5df2aae1d53503552d56
Rinat <rinat.rosenberg.kima@gmail.com>
2018-04-12 16:24:00 +0300

    gridlayout

--- dyslexia_app/table_try1.py
+++ dyslexia_app/table_try1.py
@@ -4,25 +4,21 @@ from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.gridlayout import GridLayout
+from kivy.uix.button import Button
 
 class TutorialApp(App):
     def build(self):
-        b=BoxLayout(orientation='vertical')
-        t=TextInput(font_size=50,
-                    size_hint_y=None,
-                    height=50,
-                    width=50,
-                    text='default')
-        l = Label(text='default', font_size=100)
-        l2 = Label(text='default', font_size=100)
-
-        t.bind(text=l.setter('text1'))
-        b.add_widget(t)
-        b.add_widget(l)
-        b.add_widget(l2)
+        layout = GridLayout(cols=3, row_force_default=True, row_default_height=40)
+        layout.add_widget(Button(text='Hello 1', size_hint_x=None, width=100))
+        layout.add_widget(Button(text='World 1'))
+        layout.add_widget(Button(text='?'))
+        layout.add_widget(Button(text='Hello 2', size_hint_x=None, width=100))
+        layout.add_widget(Button(text='World 2'))
+        layout.add_widget(Button(text='?'))
 
 
-        return b
+        return layout
 
 if __name__ == "__main__":
     TutorialApp().run()
