cb0c3628888051fe7d94e1644461c91434edefc5
Rinat <rinat.rosenberg.kima@gmail.com>
2018-04-16 10:53:02 +0300

    Rinat added scrollbar example

--- dyslexia_app/table_try1.py
+++ dyslexia_app/table_try1.py
@@ -6,19 +6,21 @@ from kivy.uix.textinput import TextInput
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.button import Button
+from kivy.uix.scrollview import ScrollView
+from kivy.core.window import Window
+
 
 class TutorialApp(App):
     def build(self):
         layout = GridLayout(cols=3, row_force_default=True, row_default_height=40)
-        layout.add_widget(Button(text='Hello 1', size_hint_x=None, width=100))
-        layout.add_widget(Button(text='World 1'))
-        layout.add_widget(Button(text='?'))
-        layout.add_widget(Button(text='Hello 2', size_hint_x=None, width=100))
-        layout.add_widget(Button(text='World 2'))
-        layout.add_widget(Button(text='?'))
-
-
-        return layout
+        for i in range(1,20):
+            layout.add_widget(Button(text='Hello '+str(i), size_hint_x=None, width=100))
+            layout.add_widget(Button(text='World '+str(i)))
+            layout.add_widget(Button(text='?'))
+        #return layout
+        root = ScrollView(size_hint=(1, None), size=(Window.width, Window.height))
+        root.add_widget(layout)
+        return root
 
 if __name__ == "__main__":
     TutorialApp().run()
