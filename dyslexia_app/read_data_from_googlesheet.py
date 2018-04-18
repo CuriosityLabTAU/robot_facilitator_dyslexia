3110addffa1060bd9c5ae4fad7335383ce835dff
PTorr <torrpolakow@gmail.com>
2018-04-18 14:29:52 +0300

    Rinat added read_write_json_file

---
+++ dyslexia_app/read_data_from_googlesheet.py
@@ -0,0 +1,35 @@
+"""
+Shows basic usage of the Sheets API. Prints values from a Google Spreadsheet.
+"""
+from __future__ import print_function
+#from apiclient.discovery import build #bad
+#sudo pip install --upgrade google-api-python-client
+from googleapiclient.discovery import build #good
+from httplib2 import Http
+from oauth2client import file, client, tools
+
+# Setup the Sheets API
+SCOPES = 'https://www.googleapis.com/auth/spreadsheets.readonly'
+store = file.Storage('credentials.json')
+creds = store.get()
+if not creds or creds.invalid:
+    flow = client.flow_from_clientsecrets('client_secret.json', SCOPES)
+    creds = tools.run_flow(flow, store)
+service = build('sheets', 'v4', http=creds.authorize(Http()))
+
+# Call the Sheets API
+#SPREADSHEET_ID = '1BxiMVs0XRA5nFMdKvBdBZjgmUUqptlbs74OgvE2upms'
+SPREADSHEET_ID = '1xtlwZ2EGO18lPJCNV2IWpvhSFblzFLyyHdka01mAdIs'
+
+#RANGE_NAME = 'Class Data!A2:E'
+RANGE_NAME = 'single!A2:B'
+result = service.spreadsheets().values().get(spreadsheetId=SPREADSHEET_ID,
+                                             range=RANGE_NAME).execute()
+values = result.get('values', [])
+if not values:
+    print('No data found.')
+else:
+    print('word, response:')
+    for row in values:
+        # Print columns A and E, which correspond to indices 0 and 4.
+        print('%s, %s' % (row[1], row[1]))
