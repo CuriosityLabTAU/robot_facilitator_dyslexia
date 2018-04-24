c2ca96e11711832da0ab72421732eb63b2369ca2
gorengordon <goren@gorengordon.com>
2018-04-24 12:01:29 +0300

    Rinat updated dyslexia_app.py

--- dyslexia_app/dyslexia_app.py
+++ dyslexia_app/dyslexia_app.py
@@ -19,7 +19,7 @@ class ScreenDyslexia(Screen):
         def __init__(self, the_app):
             self.the_app = the_app
             super(Screen, self).__init__()
-            self.add_words()
+            #self.add_words()
             # self.ids["title"].text = "test"
             #self.ids["text_1"].bind(text=HebrewManagement.text_change)
             #self.ids["text_2"].bind(text=HebrewManagement.text_change)
@@ -34,7 +34,7 @@ class ScreenDyslexia(Screen):
                 word_i = dyslexia_single_data['word'][i]
                 response_i = dyslexia_single_data['response'][i]
                 print(word_i, response_i)
-                lbl_word = Label(font_name='fonts/OpenSansHebrew-Bold.ttf', text=word_i, size_hint_y=None, height=20)
+                lbl_word = Label(font_name='fonts/OpenSansHebrew-Bold.ttf', text=word_i)#, size_hint_y=None, height=20)
                 lbl_response = Label(font_name='fonts/the_font.ttf', text=response_i, size_hint_y=None, height=20)
                 # btn_response = Button(text=str(response_i), size_hint_y=None, height=40)
                 layout.add_widget(lbl_word)
@@ -45,18 +45,19 @@ class ScreenDyslexia(Screen):
 
 class DyslexiaApp(App):
     def build(self):
-        layout = GridLayout(cols=3, row_force_default=True, row_default_height=40)
-        for i in range(1,20):
-            layout.add_widget(Button(text='Hello '+str(i), size_hint_x=None, width=100))
-            layout.add_widget(Button(text='World '+str(i)))
-            layout.add_widget(Button(text='?'))
+        #layout = GridLayout(cols=3, row_force_default=True, row_default_height=40)
 
-            self.the_app = self
-            self.screen_manager = MyScreenManager()
-            screen_hebrew = ScreenDyslexia(self)
-            self.screen_manager.add_widget(screen_hebrew)
-            self.screen_manager.current = 'ScreenDyslexia'
-            return self.screen_manager
+        #for i in range(1,20):
+        #    layout.add_widget(Button(text='Hello '+str(i), size_hint_x=None, width=100))
+        #    layout.add_widget(Button(text='World '+str(i)))
+        #    layout.add_widget(Button(text='?'))
+
+        self.the_app = self
+        self.screen_manager = MyScreenManager()
+        screen_dyslexia = ScreenDyslexia(self)
+        self.screen_manager.add_widget(screen_dyslexia)
+        self.screen_manager.current = 'ScreenDyslexia'
+        return self.screen_manager
 
 
 if __name__ == "__main__":
