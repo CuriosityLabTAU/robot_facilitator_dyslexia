9c91f9cf3a226e2712008903440b59481e345686
gorengordon <goren@gorengordon.com>
2018-04-30 13:58:38 +0300

    Rinat added tefel table and single table

--- dyslexia_app/dyslexia_app.py
+++ dyslexia_app/dyslexia_app.py
@@ -21,42 +21,64 @@ class MyScreenManager(ScreenManager):
     the_app = None
 
 
-class ScreenDyslexiaSingle (Screen):
+class ScreenDyslexia (Screen):
 
-        def __init__(self, the_app):
-            self.the_app = the_app
-            super(Screen, self).__init__()
-            self.add_words_single()
-            # self.ids["title"].text = "test"
-            #self.ids["text_1"].bind(text=HebrewManagement.text_change)
-            #self.ids["text_2"].bind(text=HebrewManagement.text_change)
-            # self.ids["audience_list_group_2"].bind(text=HebrewManagement.text_change)
-            # self.ids["audience_list_group_3"].bind(text=HebrewManagement.text_change)
+    def __init__(self, the_app):
+        self.the_app = the_app
+        super(Screen, self).__init__()
+        self.add_words_single()
+        self.add_words_tefel()
+        # self.ids["title"].text = "test"
+        #self.ids["text_1"].bind(text=HebrewManagement.text_change)
+        #self.ids["text_2"].bind(text=HebrewManagement.text_change)
+        # self.ids["audience_list_group_2"].bind(text=HebrewManagement.text_change)
+        # self.ids["audience_list_group_3"].bind(text=HebrewManagement.text_change)
 
-        def add_words_single(self):
-            layout = self.ids['gridlayout_tefel']
-            with open('dyslexia_tefel.json') as data_file:
-                dyslexia_single_data = json.load(data_file)
-            for i in range(len(dyslexia_single_data['word'])):
-                word_i = dyslexia_single_data['word'][i]
-                response_i = dyslexia_single_data['response'][i]
-                print(word_i, response_i)
-                lbl_response = LabelB(text=response_i[::-1])
-                lbl_word = LabelB(id='word' + str(i), text=word_i[::-1])  # , size_hint_y=None, height=20)
-                # btn_response = Button(text=str(response_i), size_hint_y=None, height=40)
-                spinner = Spinner(id='condition_spinner', text='type', font_size=16, sync_height = True,
-                                        background_color=(0, 0, 0, 1), font_name= 'fonts/the_font.ttf', values= ('1','2','3','4'), height=20, on_text= self.the_app.condition_selected)
-                layout.add_widget(spinner)
-                layout.add_widget(lbl_response)
-                layout.add_widget(lbl_word)
-                #self.ids['word' + str(i)].bind(text=HebrewManagement.text_change)
+    def add_words_single(self):
+        layout = self.ids['gridlayout_single']
+        with open('dyslexia_single.json') as data_file:
+            dyslexia_single_data = json.load(data_file)
+        for i in range(len(dyslexia_single_data['word'])):
+            word_i = dyslexia_single_data['word'][i]
+            response_i = dyslexia_single_data['response'][i]
+            print(word_i, response_i)
+            lbl_response = LabelB(text=response_i[::-1])
+            lbl_word = LabelB(id='word' + str(i), text=word_i[::-1])  # , size_hint_y=None, height=20)
+            # btn_response = Button(text=str(response_i), size_hint_y=None, height=40)
+            spinner = Spinner(id='condition_spinner', text='type', font_size=16, sync_height = True,
+                                    background_color=(0, 0, 0, 1), font_name= 'fonts/the_font.ttf', values= ('1','2','3','4'), height=20, on_text= self.the_app.condition_selected)
+            layout.add_widget(spinner)
+            layout.add_widget(lbl_response)
+            layout.add_widget(lbl_word)
+            #self.ids['word' + str(i)].bind(text=HebrewManagement.text_change)
+
+
+    def add_words_tefel(self):
+        layout = self.ids['gridlayout_tefel']
+        with open('dyslexia_tefel.json') as data_file:
+            dyslexia_single_data = json.load(data_file)
+        for i in range(len(dyslexia_single_data['word'])):
+            word_i = dyslexia_single_data['word'][i]
+            response_i = dyslexia_single_data['response'][i]
+            print(word_i, response_i)
+            lbl_response = LabelB(text=response_i[::-1])
+            lbl_word = LabelB(id='word' + str(i), text=word_i[::-1])  # , size_hint_y=None, height=20)
+            # btn_response = Button(text=str(response_i), size_hint_y=None, height=40)
+            spinner = Spinner(id='condition_spinner', text='type', font_size=16, sync_height=True,
+                              background_color=(0, 0, 0, 1), font_name='fonts/the_font.ttf', values=('1', '2', '3', '4'),
+                              height=20, on_text=self.the_app.condition_selected)
+            layout.add_widget(spinner)
+            layout.add_widget(lbl_response)
+            layout.add_widget(lbl_word)
+
+            # self.ids['word' + str(i)].bind(text=HebrewManagement.text_change)
 
 
 
-        def add_spinner(self):
-            spinner = LoggedSpinner (id= 'condition_spinner', text= 'condition', font_size= 16,
-                                     background_color= (0.2,0.2,0.2,1), font_name= 'fonts/the_font.ttf', values= ('1','2','3','4'), height=20)
-            return spinner
+    def add_spinner(self):
+        spinner = LoggedSpinner (id= 'condition_spinner', text= 'condition', font_size= 16,
+                                 background_color= (0.2,0.2,0.2,1), font_name= 'fonts/the_font.ttf', values= ('1','2','3','4'), height=20)
+        return spinner
 
 class DyslexiaApp(App):
     def build(self):
@@ -68,9 +90,9 @@ class DyslexiaApp(App):
 
         self.the_app = self
         self.screen_manager = MyScreenManager()
-        screen_dyslexia_single = ScreenDyslexiaSingle(self)
-        self.screen_manager.add_widget(screen_dyslexia_single)
-        self.screen_manager.current = 'ScreenDyslexiaSingle'
+        screen_dyslexia = ScreenDyslexia(self)
+        self.screen_manager.add_widget(screen_dyslexia)
+        self.screen_manager.current = 'ScreenDyslexia'
         return self.screen_manager
 
     def condition_selected(self):
@@ -87,11 +109,11 @@ class DyslexiaApp(App):
     def change_screen(self, screen_name):
         print ('state:', 'screen_name', screen_name)
         if (screen_name == 'single'):
-            self.screen_manager.get_screen('ScreenDyslexiaSingle').ids['single_content'].opacity = 1
+            self.screen_manager.get_screen('ScreenDyslexia').ids['single_content'].opacity = 1
         elif (screen_name == 'tefel'):
-            self.screen_manager.get_screen('ScreenDyslexiaSingle').ids['single_content'].opacity = 0
+            self.screen_manager.get_screen('ScreenDyslexia').ids['single_content'].opacity = 0
         elif (screen_name == 'summary'):
-            self.screen_manager.get_screen('ScreenDyslexiaSingle').ids['single_content'].opacity = 0
+            self.screen_manager.get_screen('ScreenDyslexia').ids['single_content'].opacity = 0
 
 
 
